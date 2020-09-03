8347f7524a8540967d2e65fa4768e950246d9211
Michal Szczepanski <lunnier@gmail.com>
2020-09-03 23:54:29 +0200

    changing Fn component for Class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <p>Tekstowa Gra Fabularna</p>
-     <h4>Oude Aard</h4>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//      <p>Tekstowa Gra Fabularna</p>
+//      <h4>Oude Aard</h4>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  state = {}
+  render() {
+    return (
+      <div className="App">
+        <p>Tekstowa Gra Fabularna</p>
+        <h4>Oude Aard</h4>
+      </div>
+    );
+  }
 }
+
 
 export default App;
