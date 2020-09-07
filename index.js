325f6b61e8c92003e18dde19c9065871b1800302
Michal Szczepanski <lunnier@gmail.com>
2020-09-07 22:20:18 +0200

    bodyParser config

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ const db = admin.firestore()
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
