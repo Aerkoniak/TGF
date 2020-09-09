bf34caf9f1418d6f3e37cf912289d2455b789c8e
Michal Szczepanski <lunnier@gmail.com>
2020-09-09 10:35:15 +0200

    build folder ignored

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
 import './css/App.css';
+
 import { BrowserRouter } from 'react-router-dom';
 import { connect } from 'react-redux'
 
