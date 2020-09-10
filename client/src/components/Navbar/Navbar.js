bcd8096fac00eb3a01d2717adb316ca984a3a713
Michal Szczepanski <lunnier@gmail.com>
2020-09-10 15:47:45 +0200

    desktop nav added and styled

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -8,17 +8,24 @@ const Navbar = ( { isLeftHanded } ) => {
     return (
         <>
             <nav className="mobile">
+                {/* <button className="navburger"><i className="fas fa-bars"></i></button> */}
                 <NavLink exact className={isLeftHanded ? "navMob left" : "navMob"} to="/">H</NavLink>
                 <NavLink className={isLeftHanded ? "navMob left" : "navMob"} to="/sessions">S</NavLink>
+                <NavLink className={isLeftHanded ? "navMob left" : "navMob"} to="/mails">P</NavLink>
                 <NavLink className={isLeftHanded ? "navMob left" : "navMob"} to="/tavern">K</NavLink>
                 <NavLink className={isLeftHanded ? "navMob left" : "navMob"} to="/charakter">KP</NavLink>
                 <NavLink className={isLeftHanded ? "navMob left" : "navMob"} to="/settings">U</NavLink>
             </nav>
             <nav className="desktop">
-                <p>Desktop</p>
+                <NavLink to="/" exact className="navDesk">Herold</NavLink>
+                <NavLink to="/sessions" className="navDesk">Sesje</NavLink>
+                <NavLink to="/mails" className="navDesk">Poczta</NavLink>
+                <NavLink to="/tavern" className="navDesk">Karczmy</NavLink>
+                <NavLink to="/charakter" className="navDesk">Karta Postaci</NavLink>
+                <NavLink to="/settings" className="navDesk">Ustawienia</NavLink>
             </nav>
         </>
-    );
+    ); 
 }
 
 const mapStateToProps = state => ({
