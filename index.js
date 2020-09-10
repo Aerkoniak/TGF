734f71e7ade107383d9bf2771c08b48ab23912d0
Michal Szczepanski <lunnier@gmail.com>
2020-09-10 15:46:43 +0200

    website/link now is served properly

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
