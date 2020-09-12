41ff3ddee648bba92711a3d953a6a45429194fbf
Michal Szczepanski <lunnier@gmail.com>
2020-09-12 14:39:51 +0200

    actions for leftHand utillities created

--- client/src/data/actions/generalActions.js
+++ client/src/data/actions/generalActions.js
@@ -1,1 +1,17 @@
 // plik dla akcji ogólnych
+import Cookies from 'js-cookie';
+
+export const setHandCoockie = argument => dispatch => {
+    const cookie = Cookies.get('isLeftHanded');
+    if (cookie === "true") {
+        dispatch({type: "TOGGLE_HAND", value: true})
+    }
+}
+
+export const toggleHand = ( argument ) => (dispatch) => {
+    Cookies.remove('isLeftHanded');
+    dispatch({type: "TOGGLE_HAND", value: argument});
+    Cookies.set('isLeftHanded', argument, {expires: 1});
+}
+
+ 
