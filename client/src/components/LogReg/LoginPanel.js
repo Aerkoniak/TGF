715b8cbc32656f0e132f2fce8a06b7f0a23c0a44
Michal Szczepanski <lunnier@gmail.com>
2020-09-15 18:03:53 +0200

    toggleForms fn created

--- client/src/components/LogReg/LoginPanel.js
+++ client/src/components/LogReg/LoginPanel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux'
 
 import RegisterForm from './RegisterForm'
@@ -7,19 +7,36 @@ import LoginForm from './LoginForm'
 
 const LoginPanel = ({ logIn }) => {
 
+    const [isLogFormVisible, setLogVisibility] = useState(true);
+    const [isRegFormVisible, setRegVisibility] = useState(true);
+
+    const toggleFormsVisibility = (e) => {
+        if (e.target.className === "logSent") {
+            setLogVisibility(!isLogFormVisible)
+            if (isRegFormVisible) {
+                setRegVisibility(false)
+            }
+        } else {
+            setRegVisibility(!isRegFormVisible);
+            if (isLogFormVisible) {
+                setLogVisibility(false)
+            }
+        }
+    }
+
     return (
         <section className="loginPanel">
             <p className="loadPage">Oude Aard</p>
             <p className="loadPage">Tekstowa Gra Fabularna</p>
-            
+
             <div className="logPanel">
                 <div className="login" >
-                    <p className="logSent">Zaloguj</p>
-                    <LoginForm loginClassName="loginForm" />
+                    <p className="logSent" onClick={toggleFormsVisibility}>Zaloguj</p>
+                    <LoginForm loginClassName={isLogFormVisible ? "loginForm" : "loginForm hidden"} />
                 </div>
                 <div className="reg">
-                    <p className="regSent">Zarejestruj</p>
-                    <RegisterForm formClassName="registerForm" />
+                    <p className="regSent" onClick={toggleFormsVisibility}>Zarejestruj</p>
+                    <RegisterForm formClassName={isRegFormVisible ? "registerForm" : "registerForm hidden"} />
                 </div>
             </div>
         </section>
