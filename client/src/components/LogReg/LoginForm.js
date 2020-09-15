407571250f1612b5c2a41ea8851ba1d4a581520b
Michal Szczepanski <lunnier@gmail.com>
2020-09-15 17:31:11 +0200

    styles for inputs changed

--- client/src/components/LogReg/LoginForm.js
+++ client/src/components/LogReg/LoginForm.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { logIn } from '../../data/actions/generalActions';
+// import { logIn } from '../../data/actions/generalActions';
 
 class LoginForm extends Component {
     state = {  }
     render() { 
         return ( 
-            <form className={this.props.loginClassName}>
-                <input type="text"/>
-                <input type="password"/>
-                <input type="submit"/>
+            <form className={this.props.loginClassName} onSubmit={(e) => e.preventDefault()}>
+                <input className="logInput" type="text"/>
+                <input className="logInput" type="password"/>
+                <input className='loginSubmit' type="submit" onClick={this.props.logIn} />
             </form>
          );
     }
 }
+
+const MapDispatchToProps = (dispatch) => {
+    return {
+        logIn: () => dispatch(logIn)
+    }
+}
  
-export default LoginForm;
+export default connect(null, MapDispatchToProps)(LoginForm);
--- client/src/components/LogReg/RegisterForm.js
+++ client/src/components/LogReg/RegisterForm.js
@@ -5,10 +5,10 @@ class RegisterForm extends Component {
     state = {  }
     render() { 
         return ( 
-            <form className={this.props.formClassName}>
-                <input type="text"/>
-                <input type="password"/>
-                <input type="password"/>
+            <form className={this.props.formClassName} onSubmit={(e) => e.preventDefault()} >
+                <input className="regInput" type="text"/>
+                <input className="regInput" type="password"/>
+                <input className="regInput" type="password"/>
                 <input className="registerSubmit" type="submit"/>
             </form>
          );
