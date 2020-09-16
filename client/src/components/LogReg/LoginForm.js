1733c2e583da18850152e7e547c497de270376a4
Michal Szczepanski <lunnier@gmail.com>
2020-09-16 11:44:52 +0200

    functional LoginForm CC created

--- client/src/components/LogReg/LoginForm.js
+++ client/src/components/LogReg/LoginForm.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { logIn } from '../../data/actions/generalActions';
+import { logIn, logInPlayer } from '../../data/actions/generalActions';
 // import { logIn } from '../../data/actions/generalActions';
 
 class LoginForm extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <form className={this.props.loginClassName} onSubmit={(e) => e.preventDefault()}>
-                <input className="logInput" type="text"/>
-                <input className="logInput" type="password"/>
-                <input className='loginSubmit' type="submit" value="Zaloguj" onClick={this.props.logIn} />
-                <p className="formInfo">Logować będzie można się nickiem, jak i nazwą postaci (po wypełnieniu kreatora).</p>
+    state = {
+        login: "",
+        password: "",
+        information: this.props.msg
+    }
+
+    fillForm = (e) => {
+        if (e.target.id === "login") {
+            this.setState({
+                login: e.target.value
+            })
+        } else if (e.target.id === "password") {
+            this.setState({
+                password: e.target.value
+            })
+        }
+    }
+    submitLogin = e => {
+        e.preventDefault();
+        let account = {};
+        account.login = this.state.login;
+        account.password = this.state.password;
+        this.props.logInPlayer(account);
+    }
+
+    render() {
+        return (
+            <form className={this.props.loginClassName} onSubmit={this.submitLogin}>
+                <input className="logInput" type="text" id="login" onChange={this.fillForm} />
+                <input className="logInput" type="password" id="password" onChange={this.fillForm} />
+                <input className='loginSubmit' type="submit" value="Zaloguj" onSubmit={this.submitLogin} />
+                    <p className="formInfo">Logowanie działa o ile wpiszecie poprawne dane. Przy błędzie nie wyrzuca błędu nigdzie poza konsolą. Sorry.</p>                
             </form>
-         );
+        );
     }
 }
 
+const MapStateToProps = state => ({
+    msg: state.player.msg
+})
+
 const MapDispatchToProps = (dispatch) => {
     return {
-        logIn: () => dispatch(logIn)
+        logInPlayer: (account) => dispatch(logInPlayer(account))
     }
 }
- 
-export default connect(null, MapDispatchToProps)(LoginForm);
+
+export default connect(MapStateToProps, MapDispatchToProps)(LoginForm);
