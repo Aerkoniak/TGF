9434595c6b4cabbdcf3113b356da47354391d1f1
Michal Szczepanski <lunnier@gmail.com>
2020-09-18 14:04:10 +0200

    storiesReducer created and implemented

--- client/src/data/reducers/storiesReducer.js
+++ client/src/data/reducers/storiesReducer.js
@@ -1,1 +1,28 @@
-// plik dla reducera dotyczącego stories
+const initState = { stories: [{
+    author: {name: "Aer", id: 0, rank: 0}, chapters: [], title: "Witajcie", startDate: "Czwartek, 17.09.2020, 15:21:14", id: 1600349041134, openMsg: `Wiadomość otwierająca sesje. Czyli teraz nie bardzo mam co w niej napisać, ponieważ przywitałem was już w tytule, a niczego więcej do przekazania chyba nie mam.`
+}, ], downloadNeed: false };
+
+const storiesReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'FETCH_START': 
+        return {
+            ...state,
+            fetch_status: "fetching"
+        };
+        case 'FETCH_COMPLETE': 
+        return {
+            ...state,
+            fetch_status: "complete",
+            stories: action.stories
+        };
+        case 'DOWNLOAD_NEED': 
+        return {
+            ...state,
+            downloadNeed: action.payload
+        }
+
+        default:
+            return state
+    }
+}
+export default storiesReducer;
