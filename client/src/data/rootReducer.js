88e6440ad3720f683d7fc4c7d4682cebc4a6d096
Michal Szczepanski <lunnier@gmail.com>
2020-09-18 14:04:24 +0200

    connecting storiesReducer with root

--- client/src/data/rootReducer.js
+++ client/src/data/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
-import playerReducer from './reducers/playerReducer'
+import playerReducer from './reducers/playerReducer';
+import storiesReducer from './reducers/storiesReducer';
 
 const rootReducer = combineReducers({
     player: playerReducer,
+    stories: storiesReducer
 })
 
 export default rootReducer;
