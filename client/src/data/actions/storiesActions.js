1e395d5ee9cd2b965208088e465e97a8cac8e5f6
Michal Szczepanski <lunnier@gmail.com>
2020-09-21 19:52:06 +0200

    Seen action inplemented

--- client/src/data/actions/storiesActions.js
+++ client/src/data/actions/storiesActions.js
@@ -5,22 +5,26 @@ export const fetchStories = () => dispatch => {
     dispatch({ type: 'FETCH_START' });
     axios.post('/stories-fetch')
         .then(res => {
-            console.log(res.data.storiesArray);
             let stories = res.data.storiesArray;
-            dispatch({ type: 'FETCH_COMPLETE', stories});
+            dispatch({ type: 'FETCH_COMPLETE', stories });
         })
 }
 
 export const addChapter = (chapter) => dispatch => {
     chapter.replyDate = createDate();
     chapter.id = new Date().getTime();
-    console.log(chapter);
-    axios.post('/stories-update', {chapter})
-    .then(res => {
-        console.log(res.data);
-        if (res.data.saved) {
-            dispatch({ type: 'DOWNLOAD_NEED', payload: true });
-            dispatch({ type: 'DOWNLOAD_NEED', payload: false });
-        }
-    })
+    axios.post('/stories-update', { chapter })
+        .then(res => {
+            if (res.data.saved) {
+                dispatch({ type: 'DOWNLOAD_NEED', payload: true });
+                dispatch({ type: 'DOWNLOAD_NEED', payload: false });
+            }
+        })
+}
+
+export const changeSeenInSession = (id, refID) => dispatch => {
+    let seen = {};
+    seen.id = id;
+    seen.refID = refID;
+    axios.post('/stories-update', { seen })
 }
