cc91be9f91eeb8c25375d64af7125bd96f5ccdcd
Michal Szczepanski <lunnier@gmail.com>
2020-09-24 22:22:25 +0200

    charactersReducer added

---
+++ client/src/data/reducers/charactersReducer.js
@@ -0,0 +1,23 @@
+// const initState = {characters: [{
+//     id: 0, name: "Frederick", rank: 0, profile: "Kilkuzdaniowy opis, określający postać."
+// }]}
+const initState = {characters: []}
+
+
+const charactersReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'FETCH_CHAR_START':
+            return{
+                ...state,
+                fetch_characters: "fetching"
+            };
+        case 'FETCH_CHAR_SUCCESS':
+            return {
+                ...state,
+                characters: action.payload
+            };
+        default:
+            return state
+    }
+}
+export default charactersReducer;
