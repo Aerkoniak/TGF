55c2a89cf9d56f7177a94476e352302233025b85
Michal Szczepanski <lunnier@gmail.com>
2020-09-27 13:58:01 +0200

    NewMessageCreator implemented

--- client/src/components/pages/MailPage.js
+++ client/src/components/pages/MailPage.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const MailPage = () => {
-    return ( 
+import ProfileViewer from '../ProfileViewer/ProfileViewer';
+import NewMessageCreator from '../NewMessageCreator/NewMessageCreator';
+
+
+const MailPage = ({ mails, msg }) => {
+
+    
+
+    const [newMessage, writeNewMessage] = useState(false)
+
+    useEffect(() => {
+        if (msg) {
+            writeNewMessage(false)
+        }
+    }, [msg])
+
+    const mailList = mails.map(mail => ((
+        <div className="oneMail" key={mail.id}>
+            <Link to={`/mails/id${mail.id}`}><p className="test">{mail.title}</p></Link>
+            <p className="test">{mail.startDate}</p>
+            <p className="test">{mail.sender.name}</p>
+        </div>
+    ))).reverse()
+    return (
         <section className="mailPage mainPage">
-            <p className="test">Poczta zarówno ta prywatna jak i ta fabularna</p>
-            <p className="test">Dla wygody czytania wyświetlane w kolejności: ostatnie u góry</p>
-            <p className="test"></p>
-            <p className="test"></p>
+            <h4 className="note">Poczta:</h4>
+            <button className="newMessageCreatorButton" onClick={() => writeNewMessage(!newMessage)} >Rozpocznij nową konwersację</button>
+            {newMessage ? <NewMessageCreator /> : null}
+            <div className="mails">
+                {mailList}
+            </div>
+
+
+            <ProfileViewer />
+
         </section>
-     );
+    );
 }
- 
-export default MailPage;
+
+const MapStateToProps = state => ({
+    mails: state.mails.mails,
+    msg: state.player.msg,
+})
+
+export default connect(MapStateToProps)(MailPage);
