05ad9f9d9958f42baa8ca879c88e6337ecff2832
Michal Szczepanski <lunnier@gmail.com>
2020-09-29 09:54:01 +0200

    SendMailReply and ChangeSeen implemented

--- client/src/data/actions/mailsActions.js
+++ client/src/data/actions/mailsActions.js
@@ -13,8 +13,30 @@ export const sendMail = message => dispatch => {
                 dispatch({ type: 'DOWNLOAD_NEED', payload: false });
                 dispatch({ type: 'CLEAN_MSG' });
             }
-
         })
+}
+
+export const sendMailReply = message => dispatch => {
+    message.replyDate = createDate();
+    // console.log(message);
+    axios.post('/mails-update', { message })
+        .then(res => {
+            if (res.data.saved) {
+                let id = message.author.id
+                axios.post('/mails-fetch', { id })
+                    .then(res => {
+                        let mails = res.data.mailsArray;
+                        dispatch({ type: 'MAIL_FETCH_COMPLETE', mails });
+                    })
+            }
+        })
+}
+
+export const changeSeenInMail  = (id, refID) => dispatch => {
+    let read = {};
+    read.id = id;
+    read.refID = refID;
+    axios.post('/mails-update', { read })
 }
 
 export const fetchMails = id => dispatch => {
