f885f06846bf1ecdc2113e4027a71af497c73aa7
Michal Szczepanski <lunnier@gmail.com>
2020-10-08 18:08:33 +0200

    upgraded by givendate

--- client/src/data/usefullFN.js
+++ client/src/data/usefullFN.js
@@ -2,9 +2,57 @@
 
 export const createDate = (givenDate) => {
     let date = new Date();
+
+    if (givenDate) {
+        let year = Number(givenDate.slice(0, 4))
+        let month = Number(givenDate.slice(5, 7))
+  
+        let day = Number(givenDate.slice(8, 10))
+        let hour = Number(givenDate.slice(12, 14))
+        let minute = Number(givenDate.slice(15, 17))
+        let second = 0;
+        let milisecond = 0;
+  
+
+        date = new Date(year, month-1, day, hour, minute, second, milisecond)
+        console.log(date)
         let dayIndex = date.getDay();
-        let timeString = date.toLocaleString(); 
+        let timeString = date.toLocaleString();
+        console.log(timeString)
+
+        let dayName = "";
+
+        switch (dayIndex) {
+            case 0:
+                dayName = "Niedziela";
+                break;
+            case 1:
+                dayName = "Poniedziałek"
+                break;
+            case 2:
+                dayName = "Wtorek"
+                break;
+            case 3:
+                dayName = "Środa"
+                break;
+            case 4:
+                dayName = "Czwartek"
+                break;
+            case 5:
+                dayName = "Piątek"
+                break;
+            case 6:
+                dayName = "Sobota"
+                break;
+        };
+        const nextTurnDate = `${dayName}, ${timeString}`;
+        return nextTurnDate;
+
+    } else {
+        let dayIndex = date.getDay();
+        let timeString = date.toLocaleString();
         let day = "";
+
         switch (dayIndex) {
             case 0:
                 day = "Niedziela";
@@ -30,4 +78,5 @@ export const createDate = (givenDate) => {
         };
         const startDate = `${day}, ${timeString}`;
         return startDate;
+    }
 }
