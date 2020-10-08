6d764de600abd2f934ed0af01a27361c8d674b81
Michal Szczepanski <lunnier@gmail.com>
2020-10-08 18:08:59 +0200

    case for redirect in StoryCreator

--- client/src/data/reducers/storiesReducer.js
+++ client/src/data/reducers/storiesReducer.js
@@ -1,6 +1,6 @@
 const initState = { stories: [{
-    author: {name: "Aer", id: 0, rank: 0}, chapters: [], title: "Witajcie", startDate: "Czwartek, 17.09.2020, 15:21:14", id: 1600349041134, openMsg: `Wiadomość otwierająca sesje. Czyli teraz nie bardzo mam co w niej napisać, ponieważ przywitałem was już w tytule, a niczego więcej do przekazania chyba nie mam.`
-}, ], downloadNeed: false };
+    author: {name: "Aer", id: 0, rank: 0}, chapters: [], title: "Witajcie", startDate: "Czwartek, 17.09.2020, 15:21:14", id: 1600349041134, openMsg: `Wiadomość otwierająca sesje. Czyli teraz nie bardzo mam co w niej napisać, ponieważ przywitałem was już w tytule, a niczego więcej do przekazania chyba nie mam.`, spectators: []
+}, ], downloadNeed: false, refID: false };
 
 const storiesReducer = (state = initState, action) => {
     switch (action.type) {
@@ -19,8 +19,21 @@ const storiesReducer = (state = initState, action) => {
         return {
             ...state,
             downloadNeed: action.payload
-        }
-
+        };
+       case 'STORY_CREATE_START':
+           return {
+               ...state,
+           };
+           case 'STORY_CREATE_SUCCESS':
+            return {
+                ...state,
+                id: action.id
+            };
+            case 'STORY_SEEN':
+            return {
+                ...state,
+                id: false
+            };
         default:
             return state
     }
