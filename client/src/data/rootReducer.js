019099400b36c083b1b4574b03ee858ef92d5f91
Michal Szczepanski <lunnier@gmail.com>
2020-10-12 21:12:04 +0200

    rootReducer upgraded by creator

--- client/src/data/rootReducer.js
+++ client/src/data/rootReducer.js
@@ -4,14 +4,16 @@ import playerReducer from './reducers/playerReducer';
 import storiesReducer from './reducers/storiesReducer';
 import charactersReducer from './reducers/charactersReducer';
 import mailsReducer from './reducers/mailsReducer';
-import tavernReducer from './reducers/tavernReducer'
+import tavernReducer from './reducers/tavernReducer';
+import creatorReducer from './reducers/creatorReducer';
 
 const rootReducer = combineReducers({
     player: playerReducer,
     stories: storiesReducer,
     characters: charactersReducer,
     mails: mailsReducer,
-    taverns: tavernReducer
+    taverns: tavernReducer,
+    creator: creatorReducer,
 })
 
 export default rootReducer;
