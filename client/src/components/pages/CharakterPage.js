a2774645cd23d76e76635a20896f9db2b91aa99b
Michal Szczepanski <lunnier@gmail.com>
2020-10-13 20:42:52 +0200

    resetKP button and function

--- client/src/components/pages/CharakterPage.js
+++ client/src/components/pages/CharakterPage.js
@@ -1,64 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
+import { resetCharacter } from '../../data/actions/creatorActions';
+
 // import NameCreator from '../NameCreator/NameCreator';
 import ProfileCreator from '../ProfileCreator/ProfileCreator';
 import ProfileViewer from '../ProfileViewer/ProfileViewer';
 import CharacterCreator from '../CharacterCreator/CharacterCreator';
 
 
-const CharakterPage = ({ player }) => {
+const CharakterPage = ({ player, done, resetCharacter }) => {
 
     const [isCharacterCreated, setCreation] = useState(false);
 
     useEffect(() => {
         if (player.race && player.name && player.age && player.class) setCreation(true);
-        console.log("Kreator powinien się schować.")
-    }, [player.name])
+        else if (!player.race && !player.name && !player.age && !player.class) setCreation(false);
+    }, [done])
 
-    const checkRang = (player) => {
-        switch (player.rank) {
-            case 0:
-                return (
-                    <p className="test">Administrator</p>
-                )
-                break;
-            case 2:
-                return (
-                    <p className="test">Mistrz Gry</p>
-                )
-                break;
-            case 3:
-                return (
-                    <p className="test">Mieszkaniec</p>
-                )
-                break;
-            case 4:
-                return (
-                    <p className="test">Przybysz</p>
-                )
-                break;
-
-            default:
-                break;
-        }
+    const resetKP = () => {
+        let char = player;
+        char.name = "";
+        char.age = null;
+        char.race = "";
+        char.class = "";
+        resetCharacter(char)
     }
 
     return (
         <section className="charakterPage mainPage">
             {isCharacterCreated ? <>
                 <h2 className="test">Twoja Karta Postaci</h2>
-                <p className="test">Ranga: <span className="kp">{checkRang(player)}</span> </p>
                 <p className="test">Imię: <span className="kp">{player.name}</span> </p>
                 <p className="test">Rasa: <span className="kp">{player.race}</span> </p>
                 <p className="test">Wiek: <span className="kp">{player.age}</span> lat</p>
                 <p className="test">Klasa: <span className="kp">{player.class}</span> </p>
+                <button className="resetCharacter" onClick={resetKP}>Zresetuj swoją KP</button>
             </> : <CharacterCreator />}
-
-
-
-
-            {/* <ProfileCreator /> */}
+            
+            <ProfileCreator />
             <ProfileViewer />
         </section>
     );
@@ -66,7 +46,12 @@ const CharakterPage = ({ player }) => {
 
 
 const MapStateToProps = state => ({
-    player: state.player.player
+    player: state.player.player,
+    done: state.creator.done
 })
 
-export default connect(MapStateToProps)(CharakterPage);
+const MapDispatchToProps = dispatch => ({
+    resetCharacter: char => dispatch(resetCharacter(char))
+})
+
+export default connect(MapStateToProps, MapDispatchToProps)(CharakterPage);
--- client/src/data/actions/creatorActions.js
+++ client/src/data/actions/creatorActions.js
@@ -1,17 +1,27 @@
 import axios from 'axios';
 
 export const setCharacter = char => dispatch => {
-    console.log(char);
     dispatch({ type: "SET_CHARACTER_START" })
     let character = char;
     character.changed = "character";
     axios.post('/edit-account', { character })
         .then(res => {
-            dispatch({ type: "SET_CHARACTER_START" })
-
             if (res.data.saved) {
                 delete character.changed;
-                dispatch({ type: "SET_CHARACTER_COMPLETE", character })
+                dispatch({ type: "SET_CHARACTER_COMPLETE", character });
             }
         })
-} 
+}
+
+export const resetCharacter = char => dispatch => {
+    dispatch({ type: "RESET_CHARACTER_START" })
+    let character = char;
+    character.changed = "character";
+    axios.post('/edit-account', { character })
+        .then(res => {
+            if (res.data.saved) {
+                delete character.changed;
+                dispatch({ type: "RESET_CHARACTER_COMPLETE", character });
+            }
+        })
+}
