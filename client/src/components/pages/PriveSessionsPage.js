b65abded9514f772da9cf0f6365d1e19c28f3856
Michal Szczepanski <lunnier@gmail.com>
2020-10-16 22:39:23 +0200

    addPrive implemented

--- client/src/components/pages/PriveSessionsPage.js
+++ client/src/components/pages/PriveSessionsPage.js
@@ -1,12 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Redirect, Link } from 'react-router-dom';
 import { connect } from 'react-redux'
 
 import ProfileViewer from '../ProfileViewer/ProfileViewer';
 import PriveStoryCreator from '../PriveStoryCreator/PriveStoryCreator';
 
-const PriveSessionsPage = ({ player }) => {
+const PriveSessionsPage = ({ player, id, priveStories }) => {
 
-    const [priveCreatorActive, setPriveCreator] = useState(false)
+    const [redirectToNewSession, activeRedirect] = useState(false);
+
+    const [priveCreatorActive, setPriveCreator] = useState(false);
+
+    useEffect(() => {
+        if (id) {
+            activeRedirect(!redirectToNewSession)
+        }
+    }, [id])
+
+    const priveStoriesLinks = priveStories.map(priveLink => {
+        let newMessage = false;
+        priveLink.players.map(spectator => {
+            if (spectator.id === player.id && spectator.seen === false) newMessage = true
+        })
+        return (
+            <div className="storyLink" key={priveLink.id}>
+                <p className="linkAuthor">{priveLink.author.name}</p>
+                <Link className={newMessage ? "linkTitle new" : "linkTitle"} id={priveLink.id} to={`/sessions/prive/id${priveLink.id}`}>{priveLink.title}</Link>
+                <p className="linkDate">{priveLink.nextTurn}</p>
+            </div>
+        )
+    })
+
     return (
         <section className="mainPage priveSessionPage">
             <button className="newStory creator" onClick={(e) => {
@@ -15,13 +39,18 @@ const PriveSessionsPage = ({ player }) => {
             }} >Stwórz nową sesję prywatną</button>
             {priveCreatorActive ? <PriveStoryCreator /> : null}
 
+            {priveStoriesLinks}
+
+            {redirectToNewSession ? <Redirect to={`/sessions/id${id}`} /> : null}
 
             <ProfileViewer />
         </section>
     );
 }
 const MapStateToProps = state => ({
-    player: state.player.player
+    player: state.player.player,
+    id: state.stories.id,
+    priveStories: state.stories.priveStories,
 })
 
 export default connect(MapStateToProps)(PriveSessionsPage);
