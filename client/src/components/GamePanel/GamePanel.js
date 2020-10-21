2bb39898ed473234e0cec9dc61d620fc04b5b1eb
Michal Szczepanski <lunnier@gmail.com>
2020-10-21 12:19:43 +0200

    changes in way we fetch data

--- client/src/components/GamePanel/GamePanel.js
+++ client/src/components/GamePanel/GamePanel.js
@@ -4,8 +4,9 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { auth } from '../../data/firebase/firebaseConfig'
 
 
-import { setHandCoockie, logInPlayer } from '../../data/actions/generalActions';
+import { setHandCoockie, logInPlayer, fetchCharactersList, updateActive } from '../../data/actions/generalActions';
 import { fetchStories } from '../../data/actions/storiesActions';
+import { fetchPriveStories } from '../../data/actions/priveStoriesActions';
 import { fetchMails } from '../../data/actions/mailsActions';
 import { fetchTavernRooms } from '../../data/actions/tavernActions';
 
@@ -24,10 +25,12 @@ import SettingsPage from '../pages/SettingsPage';
 import OneSession from '../pages/OneStory';
 import OneMail from '../pages/OneMail';
 import LogOutPage from '../pages/LogOutPage';
+import PriveSessionsPage from '../pages/PriveSessionsPage';
+import PriveStory from '../pages/PriveStory';
 
 
 
-const GamePanel = ({ player, stories, mails, downloadNeed, isLeftHanded, setHandCoockie, fetchStories, fetchMails, fetchTavernRooms, logInPlayer }) => {
+const GamePanel = ({ player, stories, mails, downloadNeed, isLeftHanded, setHandCoockie, fetchStories, fetchMails, fetchTavernRooms, logInPlayer, fetchPriveStories, priveStories, fetchCharactersList, setActive, updateActive, isLogged }) => {
 
     const [redirectToLogOut, setLogOutRedirect] = useState(false)
 
@@ -37,10 +40,6 @@ const GamePanel = ({ player, stories, mails, downloadNeed, isLeftHanded, setHand
                 let account = {}
                 account.login = user.email;
                 logInPlayer(account);
-                fetchStories();
-                setHandCoockie();
-                fetchTavernRooms();
-                fetchMails(user.email);
             } else {
                 setLogOutRedirect(!redirectToLogOut)
             }
@@ -48,11 +47,25 @@ const GamePanel = ({ player, stories, mails, downloadNeed, isLeftHanded, setHand
     }, [])
 
     useEffect(() => {
+        if (isLogged === "logged") {
+            fetchStories();
+            setHandCoockie();
+            fetchTavernRooms();
+            fetchMails(player.login);
+            fetchPriveStories(player.login);
+            fetchCharactersList();
+        }
+    }, [isLogged])
+
+    useEffect(() => {
         if (downloadNeed) {
             fetchStories();
             fetchMails(player.login);
+            fetchPriveStories(player.login);
         }
     }, [downloadNeed])
+
+
 
 
 
@@ -60,6 +73,11 @@ const GamePanel = ({ player, stories, mails, downloadNeed, isLeftHanded, setHand
     const storiesRoutes = stories.map(storyRoute => {
         return (
             <Route key={storyRoute.id} path={`/sessions/id${storyRoute.id}`} render={(routeProps) => (<OneSession {...routeProps} id={storyRoute.id} story={storyRoute} />)} />
+        )
+    })
+    const priveStoriesRoutes = priveStories.map(storyRoute => {
+        return (
+            <Route key={storyRoute.id} path={`/sessions/prive/id${storyRoute.id}`} render={(routeProps) => (<PriveStory {...routeProps} id={storyRoute.id} story={storyRoute} />)} />
         )
     })
 
@@ -75,13 +93,14 @@ const GamePanel = ({ player, stories, mails, downloadNeed, isLeftHanded, setHand
             <Switch>
                 <Route exact path='/' component={HeroldPage} />
                 <Route exact path="/sessions" component={SessionPage} />
+                <Route exact path="/sessions/prive" component={PriveSessionsPage} />
                 <Route exact path="/mails" component={MailPage} />
                 <Route path="/tavern" component={TavernPage} />
                 <Route path="/charakter" component={CharakterPage} />
                 <Route path="/settings" component={SettingsPage} />
                 <Route path='/logout' component={LogOutPage} />
 
-
+                {priveStoriesRoutes}
                 {storiesRoutes}
                 {mailsRoutes}
                 {/* <Route path='/storyCreator' render={(routeProps) => (<StoryCreator {...routeProps} player={player}></StoryCreator>)} /> */}
@@ -92,11 +111,14 @@ const GamePanel = ({ player, stories, mails, downloadNeed, isLeftHanded, setHand
     );
 }
 const MapStateToProps = state => ({
+    isLogged: state.player.isLogged,
     isLeftHanded: state.player.isLeftHanded,
     stories: state.stories.stories,
+    priveStories: state.stories.priveStories,
     mails: state.mails.mails,
     downloadNeed: state.stories.downloadNeed,
     player: state.player.player,
+    setActive: state.player.setActive,
 })
 const MapDispatchToProps = dispatch => {
     return {
@@ -104,7 +126,10 @@ const MapDispatchToProps = dispatch => {
         fetchStories: () => dispatch(fetchStories()),
         fetchMails: (id) => dispatch(fetchMails(id)),
         fetchTavernRooms: () => dispatch(fetchTavernRooms()),
-        logInPlayer: (account) => dispatch(logInPlayer(account))
+        logInPlayer: (account) => dispatch(logInPlayer(account)),
+        fetchPriveStories: mail => dispatch(fetchPriveStories(mail)),
+        fetchCharactersList: () => dispatch(fetchCharactersList()),
+        updateActive: player => dispatch(updateActive(player)),
     }
 }
 
