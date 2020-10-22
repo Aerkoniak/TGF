21c27ec8b0d72279f09cbf2679d29479ac29f535
Michal Szczepanski <lunnier@gmail.com>
2020-10-22 10:59:09 +0200

    character implemented

--- client/src/components/pages/PlayerPage.js
+++ client/src/components/pages/PlayerPage.js
@@ -1,25 +1,62 @@
 import React, { useState, useEffect } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import parse from 'html-react-parser';
 
-const PlayerPage = () => {
-    return ( 
+
+const PlayerPage = ({ character }) => {
+
+    const checkRang = (player) => {
+        switch (player.rank) {
+            case 0:
+                return (
+                    <h2 className="metrics">Administrator</h2>
+                )
+                break;
+            case 2:
+                return (
+                    <h2 className="metrics">Mistrz Gry</h2>
+                )
+                break;
+            case 3:
+                return (
+                    <h2 className="metrics">Mieszkaniec</h2>
+                )
+                break;
+            case 4:
+                return (
+                    <h2 className="metrics">Przybysz</h2>
+                )
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    return (
         <section className="mainPage playerPage">
-            <div className="metrics">
+            <div className="metricsWrap">
                 <div className="avatar">
                     tu będzie avatar
                 </div>
                 <div className="metricsData">
-                    tu będzie metryka
+                    {checkRang(character)}
+                    <h2 className="metrics">{character.name}</h2>
+                    <p className="metrics">Rasa: {character.race}</p>
+                    <p className="metrics">Z zamiłowania: {character.class}</p>
+                    <p className="metrics">Wiek: {character.age} lat</p>
                 </div>
             </div>
             <div className="profile">
-                tu będzie profil
+                <h2 className="metrics">Profil</h2>
+                {parse(character.profile)}
             </div>
+            
         </section>
-     );
+    );
 }
- 
-const MapStateToProps = state =>({
+
+const MapStateToProps = state => ({
     player: state.player.player
 })
 export default connect(MapStateToProps)(PlayerPage);
