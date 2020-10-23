aebff3bc7bb0563b9f8b85bb78976d7f79db351c
Michal Szczepanski <lunnier@gmail.com>
2020-10-23 12:17:58 +0200

    description for session added

--- client/src/components/pages/PriveSessionsPage.js
+++ client/src/components/pages/PriveSessionsPage.js
@@ -8,25 +8,25 @@ import PriveStoryCreator from '../PriveStoryCreator/PriveStoryCreator';
 const PriveSessionsPage = ({ player, id, priveStories }) => {
 
     const [redirectToNewSession, activeRedirect] = useState(false);
-
     const [priveCreatorActive, setPriveCreator] = useState(false);
 
     useEffect(() => {
         if (id) {
+            setPriveCreator(false)
             activeRedirect(!redirectToNewSession)
         }
     }, [id])
 
     const priveStoriesLinks = priveStories.map(priveLink => {
         let newMessage = false;
-        priveLink.players.map(spectator => {
+        priveLink.spectators.map(spectator => {
             if (spectator.id === player.id && spectator.seen === false) newMessage = true
         })
         return (
             <div className="storyLink" key={priveLink.id}>
                 <p className="linkAuthor">{priveLink.author.name}</p>
                 <Link className={newMessage ? "linkTitle new" : "linkTitle"} id={priveLink.id} to={`/sessions/prive/id${priveLink.id}`}>{priveLink.title}</Link>
-                <p className="linkDate">{priveLink.nextTurn}</p>
+                <p className="linkDate">{priveLink.startDate}</p>
             </div>
         )
     })
@@ -38,10 +38,15 @@ const PriveSessionsPage = ({ player, id, priveStories }) => {
                 setPriveCreator(!priveCreatorActive)
             }} >Stwórz nową sesję prywatną</button>
             {priveCreatorActive ? <PriveStoryCreator /> : null}
+            <div className="storyLink">
+                <p className="linkAuthor">Autor sesji:</p>
+                <p className="linkTitle">Tytuł:</p>
+                <p className="linkDate">Data startu:</p>
+            </div>
 
             {priveStoriesLinks}
 
-            {redirectToNewSession ? <Redirect to={`/sessions/id${id}`} /> : null}
+            {redirectToNewSession ? <Redirect to={`/sessions/prive/id${id}`} /> : null}
 
             <ProfileViewer />
         </section>
