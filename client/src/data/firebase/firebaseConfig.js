ed78f2a252b187427f8c7f096fa3383b7e5e7a31
Michal Szczepanski <lunnier@gmail.com>
2020-10-23 12:18:58 +0200

    new consts created

--- client/src/data/firebase/firebaseConfig.js
+++ client/src/data/firebase/firebaseConfig.js
@@ -1,9 +1,10 @@
-const firebase = require("firebase");
-require("firebase/firestore");
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 
 
-const FirebaseConfig = {
+export const FirebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN_FB,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -13,14 +14,17 @@ const FirebaseConfig = {
   appId: process.env.REACT_APP_APP_FB_ID,
 };
 
+firebase.initializeApp(FirebaseConfig);
 
-
-export const Firebase = firebase.initializeApp(FirebaseConfig);
+export const Firebase = () => firebase.initializeApp(FirebaseConfig);
 
 const db = firebase.firestore()
+export const auth = firebase.auth()
 
 
 export const mailsDB = db.collection('mails');
 export const storiesDB = db.collection('stories');
+export const priveStoriesDB = db.collection('stories-prive');
 export const tavernDB = db.collection('tavern');
+export const playersDB = db.collection('players');
 // export const auth = firebase.auth()
