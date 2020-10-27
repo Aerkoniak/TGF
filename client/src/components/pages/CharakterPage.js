3dc8bcfc959ec7bb8978855263462a8f19497642
Michal Szczepanski <lunnier@gmail.com>
2020-10-27 23:54:09 +0100

    creatorKP implemented

--- client/src/components/pages/CharakterPage.js
+++ client/src/components/pages/CharakterPage.js
@@ -3,42 +3,25 @@ import { connect } from 'react-redux';
 
 import { resetCharacter } from '../../data/actions/creatorActions';
 
-// import NameCreator from '../NameCreator/NameCreator';
-import ProfileCreator from '../ProfileCreator/ProfileCreator';
 import ProfileViewer from '../ProfileViewer/ProfileViewer';
-import CharacterCreator from '../CharacterCreator/CharacterCreator';
+import CreatorKP from '../CharacterCreator/CreatorKP';
 
 
 const CharakterPage = ({ player, done, resetCharacter }) => {
 
-    const [isCharacterCreated, setCreation] = useState(false);
-
-    useEffect(() => {
-        if (player.race && player.name && player.age && player.class) setCreation(true);
-        else if (!player.race && !player.name && !player.age && !player.class) setCreation(false);
-    }, [done])
 
     const resetKP = () => {
         let char = player;
-        char.name = "";
-        char.age = null;
-        char.race = "";
-        char.class = "";
         resetCharacter(char)
     }
 
     return (
         <section className="charakterPage mainPage">
-            {isCharacterCreated ? <>
-                <h2 className="test">Twoja Karta Postaci</h2>
-                <p className="test">Imię: <span className="kp">{player.name}</span> </p>
-                <p className="test">Rasa: <span className="kp">{player.race}</span> </p>
-                <p className="test">Wiek: <span className="kp">{player.age}</span> lat</p>
-                <p className="test">Klasa: <span className="kp">{player.class}</span> </p>
-                <button className="resetCharacter" onClick={resetKP}>Zresetuj swoją KP</button>
-            </> : <CharacterCreator />}
-            
-            <ProfileCreator />
+           
+            <CreatorKP />
+
+            <button className="resetCharacter" onClick={resetKP}>Zresetuj swoją KP</button>
+          
             <ProfileViewer />
         </section>
     );
