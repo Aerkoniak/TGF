6f5a563923f889b268e852de9feca0ed072cf528
Michal Szczepanski <lunnier@gmail.com>
2020-10-28 18:54:08 +0100

    TinyEditor in StoriesCreator

--- client/src/components/StoriesCreator/StoriesCreator.js
+++ client/src/components/StoriesCreator/StoriesCreator.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 // import RichEditor from '../RichEditor/RichEditor'
 import { createStory } from '../../data/actions/storiesActions';
+import TinyEditor from '../RichEditor/TinyEditor';
 
 const StoriesCreator = ({ player, createStory }) => {
 
@@ -28,16 +29,17 @@ const StoriesCreator = ({ player, createStory }) => {
     return (
         <div className="storyCreator">
             <h2 className="test">Kreator sesji globalnej</h2>
-            <form className="createStory" onSubmit={createStorySupporter}>
-                <label htmlFor="titleForStory" className="titleForStory" >Nadaj tytuł sesji:</label>
-                <input type="text" id="titleForStory" className="titleForStoryInput" value={titleValue} onChange={(e) => setTitleValue(e.target.value)} />
-                <label htmlFor="place" className="placeLabel">Wybierz dział:</label>
-                <select name="" id="place" className="placeSelect" value={placeValue} onChange={(e) => setPlaceValue(e.target.value)} >
-                    <option value="1">Dział 1</option>
-                    <option value="2">Dział 2</option>
-                    <option value="3">Dział 3</option>
+            <form className="createStory" onSubmit={e => e.preventDefault()}>
+                <label htmlFor="titleForStory" className="creatorLabel" >Nadaj tytuł sesji:</label>
+                <input type="text" id="titleForStory" className="creatorInput" value={titleValue} onChange={(e) => setTitleValue(e.target.value)} />
+                <label htmlFor="place" className="creatorLabel">Wybierz dział:</label>
+                <select name="" id="place" className="creatorInput" value={placeValue} onChange={(e) => setPlaceValue(e.target.value)} >
+                    <option value="1">Organizacyjne</option>
+                    <option value="2">Pomocnicze</option>
+                    <option value="3">Offtop</option>
                 </select>
-                <input type="submit" className="newStorySubmit" value="Zaakceptuj" />
+                <TinyEditor createStory={createStory} title={titleValue} section={placeValue} isAuthor  />
+                {/* <input type="submit" className="newStorySubmit" value="Zaakceptuj" /> */}
             </form>
         </div>
     );
