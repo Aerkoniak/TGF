dde6615ce2df66624e36a8c446c6a94612685011
Michal Szczepanski <lunnier@gmail.com>
2020-10-29 17:14:13 +0100

    logOut, setActive and CleanMSG implemented

--- client/src/data/reducers/playerReducer.js
+++ client/src/data/reducers/playerReducer.js
@@ -1,54 +1,86 @@
-const initState = { player: { login: "Aerkoniak", id: 0, name: "Ferr" , rank: 1, accountDocRef: "9cxA9hHaBFP1pWFScTFp" }, isLogged: false, isLeftHanded: false };
+const initState = { player: {}, isLogged: false, isLeftHanded: false, autoLog: false, setActive: false };
 
 
 const playerReducer = (state = initState, action) => {
     switch (action.type) {
         case 'LOG_IN_CHECKING':
-        return {
-            ...state,
-            isLogged: "checking"
-        };
-        case 'LOG_IN': 
-        return {
-            ...state,
-            isLogged: "logged", 
-            player: action.player
-        };
+            return {
+                ...state,
+                isLogged: "checking"
+            };
+        case 'LOG_IN':
+            return {
+                ...state,
+                isLogged: "logged",
+                player: action.player
+            };
         case 'LOG_IN_NOT':
             return {
                 ...state,
                 isLogged: false,
                 msg: action.msg
-            }
+            };
+        case 'LOG_OUT':
+            return {
+                ...state,
+                isLogged: false,
+                player: {}
+            };
         case 'TOGGLE_HAND':
             return {
                 ...state,
                 isLeftHanded: action.value
-        };
-        case 'REGISTER_PLAYER': 
-        return {
-            ...state,
-            isLogged: "logged", 
-            player: action.player
-        };
+            };
+        case 'REGISTER_PLAYER':
+            return {
+                ...state,
+                isLogged: "logged",
+                player: action.player
+            };
         case 'SET_PLAYER_NAME':
             return {
                 ...state,
                 msg: "Imię zmienione",
                 player: action.character
             };
-
-        case 'CLEAN_MSG': 
+        case 'SET_PLAYER_PROFILE':
+            return {
+                ...state,
+                msg: "Profil zmieniony",
+                player: action.character
+            };
+        case 'SET_MSG':
+            return {
+                ...state,
+                msg: action.msg
+            };
+        case 'CLEAN_MSG':
+            return {
+                ...state,
+                msg: false
+            };
+        case 'TOGGLE_AUTOLOG':
+            return {
+                ...state,
+                autoLog: action.payload
+            };
+        case 'SET_ACTIVE_TRUE': 
         return {
             ...state,
-            msg: false
+            setActive: true
         }
-        
+        case 'SET_ACTIVE_FALSE': 
+        return {
+            ...state,
+            setActive: false
+        }
+
+
         default:
             return state
     }
 }
 
-    
+
 
 export default playerReducer;
