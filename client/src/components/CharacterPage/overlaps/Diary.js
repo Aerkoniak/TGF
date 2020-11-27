25242a8dedda534179e1c685d6b317e8d94ac05c
Michal Szczepanski <lunnier@gmail.com>
2020-11-27 18:05:26 +0100

    CP overlaps added

---
+++ client/src/components/CharacterPage/overlaps/Diary.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Diary = () => {
+    return (
+        <>
+            <div> <h2>Dziennik MG</h2></div>
+
+        </>
+    );
+}
+
+export default Diary;
---
+++ client/src/components/CharacterPage/overlaps/Skills.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skills = () => {
+    return (
+        <>
+            <h2>Statystyki</h2>
+        </>
+    );
+}
+
+export default Skills;
