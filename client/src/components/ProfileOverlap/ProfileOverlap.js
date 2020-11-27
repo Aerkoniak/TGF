7de58c81568df6f54a146ecb7d6fe8716f2aceda
Michal Szczepanski <lunnier@gmail.com>
2020-11-27 18:07:17 +0100

    firebase listener deleted

--- client/src/components/ProfileOverlap/ProfileOverlap.js
+++ client/src/components/ProfileOverlap/ProfileOverlap.js
@@ -16,22 +16,25 @@ const ProfileOverlap = ({ profile, player, character, isLogged, editOverlap, fet
         confirmActiveEditor(false);
     }
 
-    useEffect(() => {
-        if (isLogged === "logged" && player.id === character.id) {
-            console.log("Jesteśmy w efekcie.")
-            const unsubscribe = playersDB.doc(`${player.accountDocRef}`)
-                .onSnapshot(doc => {
-                    let data = doc.data();
-                    fetchCharactersList();
-                })
-            return function cleanup() {
-                unsubscribe()
-            }
-        }
-    }, [isLogged])
+    // useEffect(() => {
+    //     if (isLogged === "logged" && player.id === character.id) {
+    //         const unsubscribe = playersDB.doc(`${player.accountDocRef}`)
+    //             .onSnapshot(doc => {
+    //                 let data = doc.data();
+    //                 fetchCharactersList();
+    //             })
+    //         return function cleanup() {
+    //             unsubscribe()
+    //         }
+    //     }
+    // }, [isLogged])
 
     return (
         <div className="profileOverlap">
+            <div className="overlapText">{parse(profile.text)} </div>
+
+            {editorActive ? <TinyEditor initialValue={profile.text} overlapName={profile.name} editOverlap={editOverlap} closeEditor={closeEditor} /> : null}
+
             {player.id === character.id ?
                 <button className="editOverlap" onClick={e => {
                     e.preventDefault();
@@ -39,9 +42,6 @@ const ProfileOverlap = ({ profile, player, character, isLogged, editOverlap, fet
                 }}>Edytuj Zakładkę</button>
                 :
                 null}
-            <div className="overlapText">{parse(profile.text)} </div>
-
-            {editorActive ? <TinyEditor initialValue={profile.text} overlapName={profile.name} editOverlap={editOverlap} closeEditor={closeEditor} /> : null}
         </div>
     );
 }
