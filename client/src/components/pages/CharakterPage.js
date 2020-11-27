79ce03902a5155302f64ea58a5846f256d2c0715
Michal Szczepanski <lunnier@gmail.com>
2020-11-27 18:06:33 +0100

    CP rebuild in overlaps way

--- client/src/components/pages/CharakterPage.js
+++ client/src/components/pages/CharakterPage.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Route, NavLink, Switch } from 'react-router-dom';
+import { playersDB } from '../../data/firebase/firebaseConfig';
 
-// import { resetCharacter } from '../../data/actions/creatorActions';
+import Summary from '../CharacterPage/overlaps/Summary'
+import Profile from '../CharacterPage/overlaps/Profile'
+import Skills from '../CharacterPage/overlaps/Skills'
+import Diary from '../CharacterPage/overlaps/Diary'
 
 import ProfileViewer from '../ProfileViewer/ProfileViewer';
-import CreatorKP from '../CharacterPage/CreatorKP';
-import CharacterPreview from '../CharacterPage/CharacterPreview';
 
 
 const CharakterPage = ({ player }) => {
@@ -19,17 +22,36 @@ const CharakterPage = ({ player }) => {
             confirmCreator(true)
         }
     }, [player])
+    useEffect(() => {
+        const unsubscribe = playersDB.doc(`${player.accountDocRef}`)
+            .onSnapshot(doc => {
+                let data = doc.data();
+                console.log("Zmiana w koncie")
+                // fetchCharactersList();
+            })
+        return function cleanup() {
+            unsubscribe()
+        }
+    }, [player])
 
 
 
     return (
         <section className="charakterPage mainPage">
+            <ul className="navigationCP">
+                <li><NavLink to="/character/summary" >Podsumowanie</NavLink></li>
+                <li><NavLink to="/character/profile" >Profil</NavLink></li>
+                <li><NavLink to="/character/skills" >Umiejętności i statystyki</NavLink></li>
+                <li><NavLink to="/character/diary" >Dziennik</NavLink></li>
+            </ul>
+            <Switch>
+                <Route path="/character/summary" component={Summary} />
+                <Route path="/character/profile" component={Profile} />
+                <Route path="/character/skills" component={Skills} />
+                <Route path="/character/diary" component={Diary} />
+            </Switch>
 
-            {isCreatorComplete ?
-                <CharacterPreview />
-                :
-                <CreatorKP />
-            }
+
 
             <ProfileViewer />
         </section>
@@ -41,8 +63,5 @@ const MapStateToProps = state => ({
     player: state.player.player,
 })
 
-const MapDispatchToProps = dispatch => ({
 
-})
-
-export default connect(MapStateToProps, MapDispatchToProps)(CharakterPage);
+export default connect(MapStateToProps)(CharakterPage);
