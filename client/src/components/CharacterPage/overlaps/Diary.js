5edf2defe1bb722558836d15db38d79942148b1c
Michal Szczepanski <lunnier@gmail.com>
2020-11-29 19:16:38 +0100

    Diary created and implemented

--- client/src/components/CharacterPage/overlaps/Diary.js
+++ client/src/components/CharacterPage/overlaps/Diary.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux'
 
-const Diary = () => {
+const Diary = ({ player, character }) => {
+
+    const [diary, setDiary] = useState({})
+    useEffect(() => {
+        if (!character) {
+            setDiary(player)
+        } else setDiary(character)
+    }, [character])
+
     return (
         <>
-            <div> <h2>Dziennik MG</h2></div>
+            <div>
+                <h2>Kronika</h2>
+                <p>{`To jest kronika ${diary.name}`}</p>
+            </div>
 
         </>
     );
 }
 
-export default Diary;
+const MapStateToProps = state => ({
+    player: state.player.player
+})
+
+export default connect(MapStateToProps)(Diary);
