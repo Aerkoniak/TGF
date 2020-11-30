cd7971302490610a981dc6a65b2b6dfc5accd886
Michal Szczepanski <lunnier@gmail.com>
2020-11-30 16:30:49 +0100

    upgraded

--- client/src/components/pages/SettingsPage.js
+++ client/src/components/pages/SettingsPage.js
@@ -1,17 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { toggleHand } from '../../data/actions/generalActions'
 import { auth } from '../../data/firebase/firebaseConfig'
-import { signOut } from '../../data/firebase/firebaseActions'
+import { signOut, sendVerification } from '../../data/firebase/firebaseActions'
+import { resetCharacter } from '../../data/actions/creatorActions';
 
 
 import LeftHandedUtility from '../LeftHandedUtility/LeftHandedUtility';
 import AutoLogUtility from '../AutoLogUtility/AutoLogUtility';
 import SetRankUtility from '../SetRankUtility/SetRankUtility';
+import DropZone from '../DropZone/DropZone';
 
-const SettingsPage = ({ signOut }) => {
-    const [redirect, setRedirect] = useState(false)
+const SettingsPage = ({ player, isLogged, signOut, sendVerification, resetCharacter }) => {
+
+    const [redirect, setRedirect] = useState(false);
+    const [verifyButtonActive, hideVerifyButton] = useState(false);
+    const [setAvatar, confirmSettingAvatar] = useState(false);
+
+    useEffect(() => {
+        auth.onAuthStateChanged(function (user) {
+            console.log(user)
+            if (!user.emailVerified) hideVerifyButton(true);
+        })
+    }, [])
+
+    const verificationSupporter = (player) => {
+        sendVerification(player.login)
+    }
+    const resetSupporter = () => {
+        let char = player;
+        resetCharacter(char);
+    }
 
     return (
         <section className="settingsPage mainPage">
@@ -19,13 +39,20 @@ const SettingsPage = ({ signOut }) => {
             <div className="desktopSetting">
                 <div className="gameSettings">
                     <h2 className="test">Ustawienia gry:</h2>
+                    {verifyButtonActive ? <button className="verifyBtn" onClick={verificationSupporter}>Wyślij e-mail weryfikacyjny</button> : null}
+
                     <AutoLogUtility />
                     <p className="logOutUtility" onClick={signOut}>Wyloguj mnie</p>
                 </div>
                 <div className="accountSettings">
                     <h2 className="test">Ustawienia konta:</h2>
-                    <SetRankUtility />
+                    {player.rank != 10 ? <SetRankUtility /> : null}
+                    <button className="resetCharacter" onClick={resetSupporter}>Zresetuj swoją KP</button>
+                    <button className="resetCharacter" onClick={e => confirmSettingAvatar(!setAvatar)} >Dodaj avatar</button>
+                    {setAvatar ? <DropZone /> : null}
+
                 </div>
+
 
 
             </div>
@@ -51,15 +78,18 @@ const SettingsPage = ({ signOut }) => {
     );
 }
 
-// const MapStateToProps = (state) => ({
-//     isLeftHanded: state.player.isLeftHanded
-// })
+const MapStateToProps = (state) => ({
+    player: state.player.player,
+    isLogged: state.player.isLogged
+})
 
 const MapDispatchToProps = dispatch => {
     return {
-        signOut: () => dispatch(signOut())
+        signOut: () => dispatch(signOut()),
+        sendVerification: email => dispatch(sendVerification(email)),
+        resetCharacter: (char) => dispatch(resetCharacter(char))
     }
 }
 
 // export default SettingsPage;
-export default connect(null, MapDispatchToProps)(SettingsPage);
+export default connect(MapStateToProps, MapDispatchToProps)(SettingsPage);
