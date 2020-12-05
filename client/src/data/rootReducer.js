75282808e6211f4801e33c181484f3b8405c7a79
Michal Szczepanski <lunnier@gmail.com>
2020-12-05 20:00:28 +0100

    rootReducer rebuild

--- client/src/data/rootReducer.js
+++ client/src/data/rootReducer.js
@@ -1,11 +1,15 @@
 import { combineReducers } from "redux";
 
+import globalStoriesReducer from './slices/globalStoriesSlice';
+import otMailsReducer from './slices/mailsSlice';
+
 import playerReducer from './reducers/playerReducer';
 import storiesReducer from './reducers/storiesReducer';
 import charactersReducer from './reducers/charactersReducer';
 import mailsReducer from './reducers/mailsReducer';
 import tavernReducer from './reducers/tavernReducer';
 import creatorReducer from './reducers/creatorReducer';
+import cpReducer from './slices/CPSlice';
 
 const rootReducer = combineReducers({
     player: playerReducer,
@@ -14,6 +18,11 @@ const rootReducer = combineReducers({
     mails: mailsReducer,
     taverns: tavernReducer,
     creator: creatorReducer,
+    // nowe reducery
+    cp: cpReducer,
+    globalStories: globalStoriesReducer,
+    m: otMailsReducer,
+
 })
 
 export default rootReducer;
